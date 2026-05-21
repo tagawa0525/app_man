bfa416581120e5858ec634a4a560de22d4b50695
Hiroaki Tagawa <tagawa0525@gmail.com>
2026-05-21 14:37:05 +0900

    init: Go用の環境設定

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "app-manager: 社内アプリケーション管理システム";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+
+            sqlc
+            go-migrate
+            templ
+
+            golangci-lint
+
+            air
+
+            sqlite
+          ];
+
+          shellHook = ''
+            export GOPATH="$PWD/.go"
+            export GOBIN="$GOPATH/bin"
+            export PATH="$GOBIN:$PATH"
+            export GOFLAGS="-mod=mod"
+
+            echo "app-manager dev shell"
+            echo "  go         : $(go version | awk '{print $3}')"
+            echo "  sqlc       : $(sqlc version)"
+            echo "  migrate    : $(migrate -version 2>&1)"
+            echo "  templ      : $(templ version 2>/dev/null || echo 'installed')"
+          '';
+        };
+      });
+}
